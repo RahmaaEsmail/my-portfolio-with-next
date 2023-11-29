0222098d13009149f6631cd39e3eadc35c8c90ba
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-11-29 14:04:12 +0200

    Make date of copyright dynamic

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,9 +1,11 @@
 import styles from './Footer.module.css';
 
 function Footer() {
+    const date = new Date().getFullYear()
+    
     return (
         <footer className={styles.footer}>
-            <p>© 2023 Copyright : <a href="https://rahmaaesmail.github.io/links-tree/" target='_blank'>Rahma Esmail</a></p>
+            <p>© {date} Copyright : <a href="https://rahmaaesmail.github.io/links-tree/" target='_blank'>Rahma Esmail</a></p>
         </footer>
     )
 }
